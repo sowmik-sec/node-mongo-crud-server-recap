26e642699e72b268fee02284520591f9bb54f20b
sowmik-sec <sowmik.sec@gmail.com>
2023-03-12 17:15:37 +0600

    user updated

--- index.js
+++ index.js
@@ -28,11 +28,37 @@ async function run() {
       const users = await cursor.toArray();
       res.send(users);
     });
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
       res.send(result);
+    });
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const user = req.body;
+      const option = { upsert: true };
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          address: user.address,
+          email: user.email,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedUser,
+        option
+      );
+      res.send(result);
+      console.log(updatedUser);
     });
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
